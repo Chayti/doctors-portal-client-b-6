951b78a96ac2d777f8a0005e96d501d6f4ae4d41
ShafiaChy <shafiachy04@gmail.com>
2022-11-01 18:04:43 +0600

    exported app instead of auth

--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,15 +1,14 @@
-import React, { createContext } from 'react';
-import useFirebase from './../../hooks/useFirebase';
+import { getAuth } from "firebase/auth";
+import React, { createContext } from "react";
+import app from "../../firebase.init";
+import useFirebase from "./../../hooks/useFirebase";
 
 export const AuthContext = createContext(null);
-
 const AuthProvider = ({ children }) => {
-    const allContexts = useFirebase();
-    return (
-        <AuthContext.Provider value={allContexts}>
-            {children}
-        </AuthContext.Provider>
-    );
+  const allContexts = useFirebase();
+  return (
+    <AuthContext.Provider value={allContexts}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -23,6 +23,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
-
-export default auth;
+export default app;
